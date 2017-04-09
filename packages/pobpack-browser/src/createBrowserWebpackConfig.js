4954e32c8d70356d722418b497c9a6fd7b35686d
Christophe Hurpeau <christophe@hurpeau.com>
2017-04-09 18:42:07 +0200

    chore: remove useless comment

--- packages/pobpack-browser/src/createBrowserWebpackConfig.js
+++ packages/pobpack-browser/src/createBrowserWebpackConfig.js
@@ -59,7 +59,6 @@ export default (target: BrowserTargetType) => (options: OptionsType) => ({
       if (typeof entry === 'string') entry = { key: entry, path: entry };
       entries[entry.key] = [
         target !== MODERN && require.resolve('babel-regenerator-runtime'),
-        // options.env !== 'production' && require.resolve('./source-map-support'),
         options.hmr && require.resolve('react-hot-loader/patch'),
         options.hmr && require.resolve('react-dev-utils/webpackHotDevClient'),
         path.join(path.resolve(options.paths.src), entry.path),
